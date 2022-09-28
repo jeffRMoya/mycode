0ca76842e5e4c3356463745f330f2c56d5b64360
jeffRMoya <jeffmoya@amazon.com>
2022-09-28 22:03:00 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+    List - simple list"""
+
+# create a list of 3 lettered animal names
+animals = ["dog", "bat", "cat", "cow", "fox"]
+
+# display the contents of that list of animals
+print(animals)
