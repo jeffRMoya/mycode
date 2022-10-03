ce211170e4308fcedb9c754f9d513e4ba9c755ad
Jeff Moya <jeffmoya@amazon.com>
2022-10-03 08:54:29 -0700

    "consolidating files"

--- challenge01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-
-    user_name = input("What is your name? ")
-    day_of_week = input("What day of the week is it? ")
-    print(f"Hello, {user_name}! Happy {day_of_week}!")
-
-main()
