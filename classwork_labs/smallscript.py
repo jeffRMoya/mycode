3ac40cec0f3158bc13ebee812dde91ad9b8c6f73
jeffRMoya <jeffmoya@amazon.com>
2022-10-04 20:50:02 +0000

    extra stuff

---
+++ classwork_labs/smallscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Author: RZFeeser
+
+This program prints some things to the screen
+"""
+def main():
+    """Our main program"""
+    my_note = 'Print this string' # define mynote
+    print(my_note) # display our string
+main()
